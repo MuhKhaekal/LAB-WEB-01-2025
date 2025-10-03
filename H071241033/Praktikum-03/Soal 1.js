707a545197e26571fa4893a26d758710bd457423
Hanifzx <muh.hanief.1974@gmail.com>
2025-10-03 15:33:11 +0800

    Tugas Praktikum 3

--- H071241033/Praktikum-03/Soal 1.js
+++ H071241033/Praktikum-03/Soal 1.js
@@ -2,7 +2,7 @@ function countEvenNumber(x, y) {
     let evenNum = []
     let jumlah = 0
     for (let i = x; i <= y; i++) {
-        if (i % 2 == 0) {
+        if (i % 2 === 0) {
             evenNum.push(i)
             jumlah += 1
         }
--- H071241033/Praktikum-03/Soal 4.js
+++ H071241033/Praktikum-03/Soal 4.js
@@ -30,36 +30,3 @@ async function main() {
 }
 
 main();
-
-
-
-
-// const angkaRahasia = Math.floor(Math.random() * 100) + 1;
-// let jumlahTebakan = 0;
-
-// function tanyaTebakan() {
-//     rl.question("Masukkan salah satu dari angka 1 sampai 100: ", (input) => {
-//         let tebakan = Number(input);
-//         jumlahTebakan++;
-
-//         if (isNaN(tebakan) || tebakan < 1 || tebakan > 100) {
-//             console.log("Tolong masukkan angka valid antara 1 sampai 100!");
-//             tanyaTebakan();
-//             return;
-//         }
-
-//         if (tebakan > angkaRahasia) {
-//             console.log("Terlalu tinggi! Coba lagi.");
-//             tanyaTebakan();
-//         } else if (tebakan < angkaRahasia) {
-//             console.log("Terlalu rendah! Coba lagi.");
-//             tanyaTebakan();
-//         } else {
-//             console.log(`Benar! Kamu menebak angka ${angkaRahasia} dalam ${jumlahTebakan} tebakan.`);
-//             rl.close();
-//         }
-//     });
-// }
-
-// tanyaTebakan();
-
